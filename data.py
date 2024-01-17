8fc59b2dc73a20b44e72b421d0a760da490dceea
arch-bert <archbert66@gmail.com>
2024-01-17 14:56:11 +0200

    works perfectly but API can't access daily data...

--- data.py
+++ data.py
@@ -3,27 +3,40 @@ import requests
 
 API_KEY = 'PNIWMQ39TBLL3JDO'
 
-function = {'monthly': ['TIME_SERIES_MONTHLY_ADJUSTED', ''],
-            'weekly': ['TIME_SERIES_WEEKLY_ADJUSTED', ''],
-            'daily': ['TIME_SERIES_DAILY_ADJUSTED', ''],
-            '60min': ['TIME_SERIES_INTRADAY', '60min'],
-            '30min': ['TIME_SERIES_INTRADAY', '30min'],
-            '15min': ['TIME_SERIES_INTRADAY', '15min'],
-            '5min': ['TIME_SERIES_INTRADAY', '5min'],
-            '1min': ['TIME_SERIES_INTRADAY', '1min'],
-            }
-
-interval = {1: '1min', 5: '5min', 15: '15min', 30: '30min', 60: '60min'}
+function = {
+    'monthly': ['TIME_SERIES_MONTHLY_ADJUSTED', 'Monthly Adjusted Time Series'],
+    'weekly': ['TIME_SERIES_WEEKLY_ADJUSTED', 'Weekly Adjusted Time Series'],
+    'daily': ['TIME_SERIES_DAILY_ADJUSTED', 'Time Series (Daily)'],
+    '60min': ['TIME_SERIES_INTRADAY', 'Time Series (60min)'],
+    '30min': ['TIME_SERIES_INTRADAY', 'Time Series (30min)'],
+    '15min': ['TIME_SERIES_INTRADAY', 'Time Series (15min)'],
+    '5min': ['TIME_SERIES_INTRADAY', 'Time Series (5min)'],
+    '1min': ['TIME_SERIES_INTRADAY', 'Time Series (1min)'],
+}
 
 symbol = input('symbol: ')
 freq = input('frequency: ')
-start_date = input('start_date: ')
-end_date = input('end_date: ')
 
 url = 'https://www.alphavantage.co/query?'
 url += f'function={function[freq][0]}'
 url += f'&symbol={symbol}'
 url += f'&apikey={API_KEY}'
 
-if not (freq == 'monthly' or freq == 'weekly' or freq == 'daily'):
+if freq not in ['monthly', 'weekly', 'daily']:
     url += f'&interval={function[freq][1]}'
+
+response = requests.get(url)
+data_json = response.json()
+
+if response.status_code == 200:
+    json_key = function[freq][1]
+    if json_key in data_json:
+        df = pd.DataFrame.from_dict(data_json[json_key], orient='index')
+        df.index = pd.to_datetime(df.index)
+        df.sort_index(inplace=True)
+    else:
+        print("Time series data not found in the response.")
+else:
+    print("Failed to fetch data, status code:", response.status_code)
+
+print(df)
