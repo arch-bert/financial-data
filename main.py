874bf1558cc61243564ec2e8178f18b09debe700
arch-bert <archbert66@gmail.com>
2024-01-19 21:18:23 +0200

    Started logic loop

---
+++ main.py
@@ -0,0 +1,21 @@
+not_valid = False
+
+while not_valid:
+
+    asset_type = input('Type of financial data: ')
+
+    if asset_type == 'stock':
+        # ...
+        break
+
+    elif asset_type == 'crypto':
+        # ...
+        break
+
+    elif asset_type == 'macro':
+        # ...
+        break
+
+    elif asset_type == 'currency':
+        # ...
+        break
