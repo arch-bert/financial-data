eb12982f0156cb50dbba6b993bbe389da3aa28fc
arch-bert <archbert66@gmail.com>
2024-01-20 22:14:46 +0200

    added function: get_time_series

---
+++ stocks/stock_prices.py
@@ -0,0 +1,34 @@
+from openbb_terminal.sdk import openbb
+
+# TODO: error handling
+# TODO: Dont let openbb.stocks.load() print to terminal
+# TODO: Print out actual start and end dates for fetched data
+
+
+def get_time_series(symbol, start_date=None, end_date=None, time_step=None):
+
+    # Load daily time series
+    if time_step is None or time_step == 'daily':
+        df = openbb.stocks.load(symbol,
+                                start_date=start_date,
+                                end_date=end_date)
+
+    elif time_step == 'monthly':
+        df = openbb.stocks.load(symbol,
+                                start_date=start_date,
+                                end_date=end_date,
+                                monthly=True)
+
+    elif time_step == 'weekly':
+        df = openbb.stocks.load(symbol,
+                                start_date=start_date,
+                                end_date=end_date,
+                                weekly=True)
+
+    elif time_step in ['1min', '5min', '15min', '30min', '60min']:
+        int(time_step.replace('min', ''))
+        df = openbb.stocks.load(symbol,
+                                start_date=start_date,
+                                end_date=end_date,
+                                interval=time_step)
+    return df
