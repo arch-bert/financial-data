6f9999a19ebce6d5542ccd17c760e4b42c1ec98e
arch-bert <archbert66@gmail.com>
2024-01-21 23:08:10 +0200

    Utilized function: print_dates_msg()

--- stocks/stock_prices.py
+++ stocks/stock_prices.py
@@ -1,5 +1,6 @@
 from openbb_terminal.sdk import openbb
 from utils.suppress_print import SuppressPrint
+from utils import data
 
 # TODO: error handling
 # TODO: Print out actual start and end dates for fetched data
@@ -7,29 +8,37 @@ from utils.suppress_print import SuppressPrint
 
 def get_time_series(symbol, start_date=None, end_date=None, time_step=None):
 
-    with SuppressPrint():
-        # Load daily time series
-        if time_step is None or time_step == 'daily':
+    # Load daily time series
+    if time_step is None or time_step == 'daily':
+        with SuppressPrint():
             df = openbb.stocks.load(symbol,
                                     start_date=start_date,
                                     end_date=end_date)
+        data.print_dates_msg(df, start_date, end_date)
 
-        elif time_step == 'monthly':
+    elif time_step == 'monthly':
+        with SuppressPrint():
             df = openbb.stocks.load(symbol,
                                     start_date=start_date,
                                     end_date=end_date,
                                     monthly=True)
+        data.print_dates_msg(df, start_date, end_date)
 
-        elif time_step == 'weekly':
+    elif time_step == 'weekly':
+        with SuppressPrint():
             df = openbb.stocks.load(symbol,
                                     start_date=start_date,
                                     end_date=end_date,
                                     weekly=True)
+        data.print_dates_msg(df, start_date, end_date)
 
-        elif time_step in ['1min', '5min', '15min', '30min', '60min']:
+    elif time_step in ['1min', '5min', '15min', '30min', '60min']:
+        with SuppressPrint():
             int(time_step.replace('min', ''))
             df = openbb.stocks.load(symbol,
                                     start_date=start_date,
                                     end_date=end_date,
                                     interval=time_step)
+            data.print_dates_msg(df, start_date, end_date)
+
     return df
